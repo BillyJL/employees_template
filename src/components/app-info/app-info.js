c120660248e1b022777cc3fd943992910e8fb4e4
Taras <tarasmikulskii@ukr.net>
2022-07-06 11:02:05 +0300

    Creating a template for the project

---
+++ src/components/app-info/app-info.js
@@ -0,0 +1,13 @@
+import './app-info.css';
+
+const AppInfo = () => {
+    return (
+        <div className="app-info">
+            <h1>Облік працівників у компанії</h1>
+            <h2>Загальна кількість працівників: </h2>
+            <h2>Премію отримують:</h2>
+        </div>
+    )
+}
+
+export default AppInfo;
