747382e41202e685cabecba904baa6ca82973cc4
Taras <tarasmikulskii@ukr.net>
2022-07-06 13:26:23 +0300

    Using props for creating employees list items

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -7,6 +7,13 @@ import EmployeesAddForm from '../employees-add-form/employees-add-form';
 import './app.css';
 
 function App() {
+
+    const data = [
+        {name: "Бонапарт Н.", salary: 800, increase: false},
+        {name: "Цезарь Ю.", salary: 3000, increase: true},
+        {name: "Черчилль В.", salary: 5000, increase: false},
+    ];
+
     return (
         <div className="app">
             <AppInfo/>
@@ -16,7 +23,7 @@ function App() {
                 <AppFilter/>
             </div>
 
-            <EmployeesList/>
+            <EmployeesList data={data}/>
             <EmployeesAddForm/>
         </div>
     )
